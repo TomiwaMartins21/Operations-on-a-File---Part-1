d076776da4515e8b824447262c375c5b66c3cd24
TomiwaMartins21 <martinstomiwa838@gmail.com>
2025-03-30 13:10:23 +0000

    Inital commit

--- Activtiy-4/main.py
+++ Activtiy-4/main.py
@@ -1,23 +1,23 @@
 # Program to copy odd lines of one file to another
 # open file in read mode
-fn = open('Codingal.txt', 'r')
+fn = open('Activtiy-4\Codingal.txt', 'r')
 
 # open other file in write mode 
-fn1 = open('CodingalUpdated.txt','w')
+fn1 = open('Activtiy-4\CodingalUpdated.txt','w')
 
 # read the content of the file line by line
 cont = fn.readlines()
 type(cont)
 for i in range(1, len(cont)+1):
-         if(i % 2 != 0):
-                  fn1.write(cont[i-1])
+        if(i % 2 != 0):
+                fn1.write(cont[i-1])
         else:
                 pass
 # close the file
 fn1.close()
 
 # open file in read mode 
-fn1 = open('CodingalUpdated.txt', 'r')
+fn1 = open('Activtiy-4\CodingalUpdated.txt', 'r')
 
 # read the content of the file
 cont1 = fn1.read()
